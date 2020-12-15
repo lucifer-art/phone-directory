3b9b34a162f94dab3ba0bf0dc98345f4669df41f
Shivang Kumar <shivang.kumar@deltaschool.in>
2020-12-15 19:39:28 +0530

    jsx diffrence with html

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ class App extends Component{
   render(){
     return(
       <div>
+        <div>
         Phone directory
+        </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+          <div>
+            <label htmlFor="textField">Name </label>
+          <input id="textField" type="text" defaultValue="Shivang"/>
+          </div>
+          
+        </div>
       </div>
     );
   }
