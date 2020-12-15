de4cbc328b01aafe46a2d880a4f528c8ac514f7d
Shivang Kumar <shivang.kumar@deltaschool.in>
2020-12-15 20:11:40 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React, { Component} from 'react';
 
 class App extends Component{
   render(){
+
     return(
-      <div>
-        <div>
-        Phone directory
-        </div>
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
-          <span>Phone</span>
-          <div>
-            <label htmlFor="textField">Name </label>
-          <input id="textField" type="text" defaultValue="Shivang"/>
-          </div>
-          
-        </div>
-      </div>
+      // <div>
+      //   {/* <div>
+      //     Phone directory
+      //   </div>
+      //   <button>Add</button>
+      //   <div>
+      //     <span>Name</span><br />
+      //     <span>Phone</span>
+      //     <div>
+      //       <label htmlFor="textField">Name </label>
+      //       <input id="textField" type="text" defaultValue={x + y} />
+      //     </div>
+
+      //   </div> */}
+      //   <div id="module">
+      //     <p>ReactJS</p>
+      //   </div>
+      // </div>
+      React.createElement("div",{id:"module"},
+      React.createElement('p',null,'ReactJS')
+      )
     );
   }
 }
