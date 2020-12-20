6dc61e88da40be41e827714b5679b4494096b5a7
Shivang Kumar <shivang.kumar@deltaschool.in>
2020-12-20 14:45:13 +0530

    added style for app page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
+import './App.css';
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div>
-        <Header>Hello World!</Header>
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
-          <span>Phone</span>
-        </div>
         {/* <div id="module">
           <p>ReactJS</p>
         </div> */}
+        <Header>Hello World!</Header>
+        <div className='component-body-container'>
+          <button className='cust-btn add'>Add</button>
+          <div className='gridView'>
+            <div className='nameText'>Name</div>
+            <div className='phoneText'>Phone</div>
+          </div>
+        </div>
+
+
       </div>
+
       // React.createElement("div",{id:"module"},
       // React.createElement('p',null,'ReactJS')
-      );
+    );
   }
 }
 // function App() {
