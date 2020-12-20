ac0dae7aa2a033c8db0fbf311c0d706285277cf9
Shivang Kumar <shivang.kumar@deltaschool.in>
2020-12-20 11:32:42 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import React, { Component} from 'react';
+import Header from './Header';
 
 class App extends Component{
   render(){
-
     return(
-      // <div>
-      //   {/* <div>
-      //     Phone directory
-      //   </div>
-      //   <button>Add</button>
-      //   <div>
-      //     <span>Name</span><br />
-      //     <span>Phone</span>
-      //     <div>
-      //       <label htmlFor="textField">Name </label>
-      //       <input id="textField" type="text" defaultValue={x + y} />
-      //     </div>
-
-      //   </div> */}
-      //   <div id="module">
-      //     <p>ReactJS</p>
-      //   </div>
-      // </div>
-      React.createElement("div",{id:"module"},
-      React.createElement('p',null,'ReactJS')
-      )
-    );
+      <div>
+        <Header></Header>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
+        {/* <div id="module">
+          <p>ReactJS</p>
+        </div> */}
+      </div>
+      // React.createElement("div",{id:"module"},
+      // React.createElement('p',null,'ReactJS')
+      );
   }
 }
 // function App() {
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = function(){
+    return(
+        <div>
+          Phone directory
+        </div>
+    )
+}
+
+// class Header extends Component {
+//   render(){
+//     return(
+//       <div>
+//           Phone directory
+//       </div>
+//     )
+//   }
+// }
+
+
+export default Header;
